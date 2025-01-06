822297eff9450bb0767af955605c4127d491ab8c
dimgray1995 <dimgray1995@gmail.com>
2025-01-06 15:58:06 +0900

    [UI/UX] 메인 로비 테스트용 레이아웃 완성입니다
    [body] https://drive.google.com/file/d/1_b9tqUurWcc-vedhhuv2IFEmr_5tjgjD/view?usp=drive_link 에셋 드라이브 업데이트 부탁드립니다

---
+++ Salon/Assets/LangtableLoader/Scripts/CSVManager.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class CSVManager : DataManager
+{
+    private static CSVManager instance;
+    public static CSVManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject("CSVManager");
+                instance = go.AddComponent<CSVManager>();
+                DontDestroyOnLoad(go);
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public override void LoadData()
+    {
+        if (dataFiles.Count == 0)
+        {
+            Debug.LogWarning("로드할 CSV 파일이 설정되지 않았습니다.");
+            return;
+        }
+
+        foreach (DataFile file in dataFiles)
+            LoadCSVFromResources(file.path);
+    }
+
+    protected override string ProcessPath(string fullPath)
+    {
+        string resourcePath = fullPath;
+        int resourcesIndex = fullPath.IndexOf("Resources/");
+
+        if (resourcesIndex != -1)
+            resourcePath = fullPath.Substring(resourcesIndex + 10);
+
+        if (resourcePath.EndsWith(".csv", System.StringComparison.OrdinalIgnoreCase))
+            resourcePath = resourcePath.Substring(0, resourcePath.Length - 4);
+
+        return resourcePath;
+    }
+
+    public List<string[]> LoadCSVFromResources(string fullPath)
+    {
+        string resourcePath = ProcessPath(fullPath);
+        List<string[]> csvData = new List<string[]>();
+
+        TextAsset csvFile = Resources.Load<TextAsset>(resourcePath);
+        if (csvFile == null)
+        {
+            Debug.LogError($"CSV 파일을 찾을 수 없습니다. 경로: {fullPath}");
+            return csvData;
+        }
+
+        string[] lines = Regex.Split(csvFile.text, "\r\n|\n|\r");
+        foreach (string line in lines)
+        {
+            if (!string.IsNullOrEmpty(line))
+                csvData.Add(line.Split(','));
+        }
+
+        csvDataSets[resourcePath] = csvData;
+        return csvData;
+    }
+
+    public bool LoadAdditionalCSV(string fullPath)
+    {
+        string resourcePath = ProcessPath(fullPath);
+        dataFiles.Add(new DataFile { path = resourcePath });
+        List<string[]> data = LoadCSVFromResources(fullPath);
+        return data != null && data.Count > 0;
+    }
+}
